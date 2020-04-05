f2cf5d7bca1d9959d403dd61292ba92966bf0804
lpcruz <laurence.p.cruz@gmail.com>
2020-04-05 15:06:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# dev-journal
+
+A node binary that auto-generates notes for you in markdown. 
+
+#### Installation
+
+```
+$ npm install -g dev-journal
+```
+
+#### Making a note
+
+Once you've successfully installed globally, you'll have access to the `dev-note` binary command. It will create a `journal` directory in your current working directory along with a markdown file with the current date:
+
+```
+cwd/
+├── journal/
+    ├── <MM-DD-YYYY>.md
+```
+
+The markdown file will already have a template with the current date:
+
+```
+### Notes <MM-DD-YYYY>
+========================
+```
---
+++ src/note.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+const fs = require('fs');
+
+/**
+ * @return {string} -- Current month name
+ */
+const getCurrentMonthName = () => {
+  const months = [ 
+    'jan',
+    'feb',
+    'mar',
+    'apr',
+    'may',
+    'jun',
+    'jul',
+    'aug',
+    'sep',
+    'oct',
+    'nov',
+    'dec'
+  ];
+  return months[new Date().getMonth()];
+};
+
+/**
+ * @return {string} -- ISO format of current date
+ */
+const getFormattedDate = () => {
+  return new Date().toISOString().split('T')[0];
+};
+
+/**
+ * 
+ * @param {*} date -- ISO format of date to be set as the note header
+ */
+const formatHeader = date => {
+  return `### Notes ${date}\n========================`;
+};
+
+/**
+ * 
+ * @param {*} month -- the current month for the notes path  
+ */
+const mkdir = month => {
+  const path = `./notes/${month}`;
+  if (!fs.existsSync(path)) {
+    console.log('Looks like this is your first time running dev-journal this month');
+    fs.mkdirSync(path, { recursive: true });
+    console.log('We made a "notes" directory for your notes. Happy writing!');
+  }
+  return path;
+}
+
+/**
+ * Generates note as a markdown file
+ */
+const note = () => {
+  const date = getFormattedDate();
+  const header = formatHeader(date);
+  const path = mkdir(getCurrentMonthName());
+  const pathToNote = `${path}/${date}.md`;
+  
+  fs.writeFile(pathToNote, header, err => {
+    if (err) throw err;
+    console.log(`${pathToNote} successfully created!`);
+  });
+};
+
+note();
